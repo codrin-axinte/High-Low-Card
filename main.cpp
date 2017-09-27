dfe27c9674410063d52189102104f7ffd35ddc37
Axinte Codrin <6564791+codrin-axinte@users.noreply.github.com>
2017-09-27 10:55:49 +0100

    AI loses, removed unused functions

--- main.cpp
+++ main.cpp
@@ -12,24 +12,18 @@ using namespace std;
 * As a user I would like to enter my name
 * Rules:
 * - The first with no cash loses
-* - If a player is right he gets full cash
-* - If both players are right they both get full cash
+* - If a player is right he gets full bet
+* - If both players are right they get half of the bet
 * - If the player is not right he loses cash
 * - If the players don't pick the same answer they can bet/raise the reward for that round
-* - Players can choose with what amount to start and set the increase/decrease rates
+* - Players can choose with what amount to start and set the increase/decrease bets
 */
-
-string Str(int value){
-  return std::to_string(value);
-}
-int Num(string value){
-  return std::stoi( value );
-}
 
 int shuffle(int min = 1, int max = 13){
     srand(time(NULL));
     return rand() % ((max-min) + 1) + min;
 }
+
 class Wallet {
   int value = 50;
 
@@ -43,12 +37,33 @@ class Wallet {
       return value;
     }
   
+    int amount(){
+      return value;
+    }
+  
     void increase(int amount){
       value += amount;
     }
     
     void decrease(int amount){
         value -= amount;
+    }
+    
+    bool has(int amount){
+      return value >= amount;
+    }
+    
+    bool greater(int amount){
+      return value > amount;
+    }
+    
+    bool lesser(int amount){
+      return value < amount;
+    }
+    
+    
+    string toString(){
+      return "£"+to_string(value);
     }
     
     bool isEmpty(){
@@ -82,6 +97,9 @@ class Player {
 class AI : public Player {
   
   private:
+  int countWins = 0;
+  int countLosts = 0;
+  
   bool getDecision(int chance = 50){
     return rand() % 100 < chance;
   }
@@ -89,11 +107,13 @@ class AI : public Player {
   public:
   bool getPickDecision(int card){
     switch(card){
-      // If the current card is 1(Ace) the AI shall know that cannot be a lower card
-      case 1:
+      case 1:    // If the current card is 1(Ace) the AI shall know that cannot be a lower card
         return true;
-        // If the current card is 13(King) the AI shall know that cannot be a higher card
-      case 13:
+      case 2:
+        return getDecision(90);
+      case 3:
+        return getDecision(80);
+      case 13:        // If the current card is 13(King) the AI shall know that cannot be a higher card
         return false;
       case 12:
         return getDecision(10);
@@ -104,12 +124,16 @@ class AI : public Player {
     return getDecision(50);
   }
   
-  bool getBetDecision(){
-    
-    
+  int getBetAmount(Wallet wallet){
+    // TODO:: Not sure if to implement
   }
   
-  bool wantsToBet(){
+  
+  bool wantsToBet(int defaultBet = 0, int loseBet = 0) {
+    if(wallet.lesser(loseBet)){
+        return getDecision(20);
+    }
+    
     return false;
   }
   
@@ -129,7 +153,7 @@ class Deck {
    public:
     string display(int number){
      if(number > 1 && number <= 10){
-        return Str(number);
+        return to_string(number);
       }
   
       switch(number){
@@ -185,7 +209,8 @@ class Output {
   
   public:
     string ask(string question){
-        cout << "[?] " << question;
+        cout << "[?] " << question << endl;
+        cout << ">>> ";
         string option;
         cin >> option;
         return option;
@@ -193,10 +218,8 @@ class Output {
     
     bool confirm(string question){
       string option = ask(question + " (y/n): ");
-      
-      return option == "y" || option == "Y" || option == "1" || option == "yes";
+      return option == "y" || option == "yes";
     }
-    
     
     void info(string message, string value){
       cout << "[-] " << message << value << endl;
@@ -220,11 +243,11 @@ class Output {
     }
     
     void won(string name, int amount){
-      cout << "[OK] " << name << " won " << Str(amount) << endl;
+      cout << "[OK] " << name << " won " << to_string(amount) << endl;
     }
     
     void lost(string name, int amount){
-      cout << "[X] " << name << " lost " << Str(amount) << endl;
+      cout << "[X] " << name << " lost " << to_string(amount) << endl;
     }
     
     void picked(string name, bool pick){
@@ -239,14 +262,56 @@ class Output {
     }
 };
 
+
+class Bet {
+  
+  private:
+      int min = 10;
+      int minLose = 0;
+      int current = 10;
+      int currentLose = 0;
+      
+  public:
+    
+    void set(int bet, int loseBet = 0){
+      min = bet;
+      minLose = loseBet;
+      current = min;
+      currentLose = loseBet;
+    }
+    
+    void rise(int amount) {
+      current += amount;
+      currentLose += amount;
+    }
+    
+    void reset() {
+      current = min;
+      currentLose = minLose;
+    }
+    
+    int amount(){
+      return current;
+    }
+    
+    int halfAmount(){
+      return current / 2;
+    }
+    
+    int loseAmount(){
+      return currentLose;
+    }
+  
+};
+
 class Game {
   private:
     bool keepAlive = true;
     int round = 0;
-    int bet = 50;
-    int loseBet = 40;
-    int roundBet = 0;
+
   public:
+    Bet bet;
+  
     bool isRunning(){
       return keepAlive;
     }
@@ -261,93 +326,100 @@ class Game {
     
     void newRound(){
       round++;
-      roundBet = bet;
+      bet.reset();
     }
     
    void reset(){
      round = 0;
    }
-   
-   void setBet(int amount){
-    bet = amount;
-    roundBet = bet;
-   }
-   
-   void setLoseBet(int amount){
-     loseBet = amount;
-   }
-   
-   void raiseBet(int amount){
-     roundBet += amount;
-   }
-   
-   
-   int getBet(){
-     return roundBet;
-   }
-   
-   int getLoseBet(){
-     return loseBet;
-   }
+
 };
+
 Output out;
 Game game;
 Deck deck;
 Player player;
 AI ai;
+
 int main(){
-  game.setBet(50);
-  game.setLoseBet(0);
-  player.setName("Player1");
+  // SETUP
+  player.setName(out.ask("What is your name? "));
   ai.setName("Player2");
+  game.bet.set(stoi(out.ask("In what bet do yo want to play? ")), 40);
+  // GAME LOOP
   while(game.isRunning()){
+    // START A NEW ROUND
     game.newRound();
     deck.pick();
     cout << "Round " << game.getRound() << endl;
-    string header = player.getName() + " wallet: " + Str(player.wallet.getValue()) + " | " + ai.getName() + " wallet: " + Str(ai.wallet.getValue());
-    out.header(header);
+    out.header(player.getName() + " wallet: " + player.wallet.toString() + " | " + ai.getName() + " wallet: " + ai.wallet.toString());
     out.info("Card: ", deck.displayFirstCard());
     bool isHigher = out.confirm("The next card is higher?");
-    bool aiHiger = ai.getPickDecision(deck.getFirstCard());
+    bool aiHigher = ai.getPickDecision(deck.getFirstCard());
     out.picked(player.getName(), isHigher);
-    out.picked(ai.getName(), aiHiger);
-    if(isHigher != aiHiger){
-      // Player can bet
-      if(out.confirm(ai.getName() + " picked the opposite of you. Do you want to bet?")){
-        string amount = out.ask("With how much you want to raise the bet? ");
-        out.info('Raising the amount with: ', amount);
-        game.riseBet(Num(amount));
-      } else if(ai.wantsToBet()) {
-        // Now the Ai wants a bet
+    out.picked(ai.getName(), aiHigher);
+    
+    // WANNA BET?
+    if(isHigher != aiHigher){
+      // CHECK WHO WANTS TO BET
+      if(out.confirm(ai.getName() + " picked the opposite of you. Do you want to bet?") || ai.wantsToBet()) {
+        string amount;
+        // MAKE SURE EVERYONE IS ABLE TO PAY THAT AMOUNT
+        do {
+           amount = out.ask("With how much you want to raise the bet? ");
+          if(stoi(amount) > player.wallet.amount()){
+            out.warning("You don't have enough enough money to bet.");
+          }
+        } while(stoi(amount) > player.wallet.amount());
+        out.info("Raising the amount with: ", amount);
+        game.bet.rise(stoi(amount));
       }
     }
+    
+    // THE MOMENT OF TRUTH
     out.info("Second card was: ", deck.displaySecondCard());
-    int bet = game.getBet();
-    if(deck.check(aiHiger)){
+    
+    bool aiIsRight = deck.check(aiHigher);
+    bool playerIsRight = deck.check(isHigher);
+
+    // IF BOTH PLAYERS ARE RIGHT THEN WE SET THE BET AT HALF
+    int bet = aiIsRight && playerIsRight ? game.bet.halfAmount() : game.bet.amount();
+    int loseAmount = game.bet.loseAmount();
+    
+    // IS AI RIGHT OR WRONG?
+    if(aiIsRight){
       ai.wallet.increase(bet);
       out.won(ai.getName(), bet);
     } else {
-      ai.wallet.decrease(game.getLoseBet());
-      out.lost(ai.getName(), game.getLoseBet());
+      ai.wallet.decrease(loseAmount);
+      out.lost(ai.getName(), loseAmount);
     }
-  
-   if(deck.check(isHigher)) {
+    
+    // IS THE PLAYER RIGHT OR WRONG?
+   if(playerIsRight) {
       player.wallet.increase(bet);
       out.won(player.getName(), bet);
     } else {
-      player.wallet.decrease(game.getLoseBet());
-      out.lost(player.getName(), game.getLoseBet());
+      player.wallet.decrease(loseAmount);
+      out.lost(player.getName(), loseAmount);
     }
-    out.blank();
     
+    // LET'S BREATH
+    out.blank();
     deck.reset();
+    
+    // ARE YOU EVEN BROKE, BRO?
+    if(ai.wallet.isEmpty()){
+      cout << "[OK] Yey! You hav won the game with " << player.wallet.amount() << " in your wallet. Congrats!" << endl;
+      game.exit();
+    }
+    
     if(player.wallet.isEmpty()){
        if(out.confirm("You're broke! You want to restart?")){
          player.wallet.reset();
          ai.wallet.reset();
          game.reset();
          deck.reset();
-         // restart game
        } else {
          game.exit();
        }
